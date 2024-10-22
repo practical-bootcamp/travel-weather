e9bfcf7de8a387e5282d39a395c335dab4c2d4f2
Alfredo Deza <alfredo@deza.pe>
2024-10-22 16:44:56 +0000

    Update OpenAPI version to 3.1.0 and fix test for Spain's June weather

--- webapp/test_main.py
+++ webapp/test_main.py
@@ -16,3 +16,12 @@ def test_countries():
         "England", "France", "Germany",
         "Italy", "Peru", "Portugal", "Spain"
     ]
+
+
+def test_spain_june():
+    response = client.get("/countries/Spain/Seville/June")
+    assert response.status_code == 200
+    assert response.json() == {
+        "high": 33.0,
+        "low": 19.0,
+    }
